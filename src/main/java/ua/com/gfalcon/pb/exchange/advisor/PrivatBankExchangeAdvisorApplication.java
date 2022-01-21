e7b70ae930e7f669774eec3bc437d09ff5f1faff
Oleksii KHALIKOV <oleksii.khalikov@gmail.com>
2022-01-21 22:48:05 +0100

    init commit

---
+++ src/main/java/ua/com/gfalcon/pb/exchange/advisor/PrivatBankExchangeAdvisorApplication.java
@@ -0,0 +1,13 @@
+package ua.com.gfalcon.pb.exchange.advisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivatBankExchangeAdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivatBankExchangeAdvisorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/gfalcon/pb/exchange/advisor/PrivatBankExchangeAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.gfalcon.pb.exchange.advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivatBankExchangeAdvisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
